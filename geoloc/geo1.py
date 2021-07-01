22cec72cb13398e88afe922ddaae95bd101d9707
Ingrid Heuer <61297025+ingridheuer@users.noreply.github.com>
2021-07-01 11:23:41 -0300

    Add files via upload

--- geoloc/geo1.py
+++ geoloc/geo1.py
@@ -1,27 +1,44 @@
 #%%
-from geopandas.array import points_from_xy
+#region LIBRERIAS
 import pandas as pd
 import numpy as np
+
+from geopandas.array import points_from_xy
+from geopandas.geodataframe import GeoDataFrame
+import pyproj #para calcular distancias
+
 import shapely as shp
-import geopandas as gpd
-import matplotlib.pylab as plt
-from matplotlib import cm
 from shapely.geometry import Point
+import geopandas as gpd
 
-# Hay que normalizar num de habitantes para el gradiente de color, no lo hice aun
-from sklearn import preprocessing
+import matplotlib.pyplot as plt
+from matplotlib import cm
 
+import folium
+from folium import Marker, GeoJson, CircleMarker
+from folium import plugins
+from folium.plugins import HeatMap
+from folium.map import *
+
+import plotly.graph_objects as go
+
+
+from tqdm import tqdm
 
 def topoint(x):
     """para cargar los points como objetos de shapely"""
     if isinstance(x, list):
-        return [shp.wkt.loads(point) for point in x]
+        return [shp.wkt.loads(point) for point in x] #si son listas
     else:
         return shp.wkt.loads(x)
 
-
+#DF de geopandas con los contornos del mapa
+world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+sistema = world.crs
+print(f'Estamos usando el sistema {sistema}')
+#endregion
 #%%
-# region armar geodfs
+# region armo GeoDF
 df = pd.read_pickle(
     "/home/ingrid/Documents/labodatos/TP_final/df_principal/df_principal.pkl"
 ).drop(
@@ -39,120 +56,198 @@ df = pd.read_pickle(
     ]
 )
 
-# con geopd genero una coordenada desde las dos columnas de lat/long
-df["point_lang"] = gpd.points_from_xy(df["Longitude_lang"], df["Latitude_lang"])
+#quedaron de señas :()
+boolean = df['Genus'].str.contains('Sign').fillna(False)
+df = df.drop(df[boolean].index)
+
+#Genero una columna Status numérico
+stat_dict = {'6b Threatened':6.5, '4 Educational':4, '5 Developing':5,
+       '3 Wider Communication':3, '10 Extinct':10, '2 Provincial':2,
+       '8b Nearly Extinct':8.5, '6a Vigorous':6, '7 Shifting':7, '8a Moribund':8,
+       '1 National':1, '9 Dormant':9.5, '9 Reawakening':9,
+       '9 Second language only':9, '0 International':0}
+
+
+df['num_status'] = df['Status'].replace(stat_dict)
+
+#completo unos valores a mano
+df.loc[[2294, 2295, 2296], "countryISO"] = "MEX"
+df.loc[[2294, 2295, 2296], "point_cn"] = "Point(-102.0 23.0)"
+
+# con geopd genero una coordenada/Point desde las dos columnas de lat/long
+df["point_lang"] = gpd.points_from_xy(df["Longitude_lang"], df["Latitude_lang"], crs='epsg:4326')
 df["num_speakers"] = df["num_speakers"].astype(int)
 
 
-# para graficar puntos necesito un df a la vez, armo uno para native:
-geodf_native = gpd.GeoDataFrame(df, geometry="point_lang").drop(
-    columns=["Latitude_lang", "Longitude_lang"]
-)
+# y creo el geoDF
+geodf = gpd.GeoDataFrame(df, geometry="point_lang")
 
-# el de externos necesito modificarlo, para tener un punto por pais:
-# tengo que explotar el df para tener una coordenada por cada pais donde se habla el idioma
-
-# query de la coord de cada pais por ISO
-query = pd.read_csv(
-    "/home/ingrid/Documents/labodatos/TP_final/df_principal/geoloc/query_coord.csv"
-).drop(columns=["countryLabel", "country"])
-
-# para mapear la coordenada al pais
-mapita = query.set_index("countryISO").to_dict()["point_cn"]
-
-df2 = df.explode("countryISO").drop(columns="point_lang")
-
-# un dato que cambie a mano porque me rompia todo
-df2.loc[[2294, 2295, 2296], "countryISO"] = "MEX"
-df2.loc[[2294, 2295, 2296], "point_cn"] = "Point(-102.0 23.0)"
-
-
-df2["point_cn"] = df2["countryISO"].replace(mapita)
-df2["point_cn"] = df2["point_cn"].apply(topoint)
-
-geodf_ext = gpd.GeoDataFrame(df2, geometry="point_cn")
-
+#endregion
 # %%
-# region MAPA LATINOAMERICA SOLO NATIVOS
+# region EJ: SUDAMERICA PLT
 
-# los contornos de los paises los cargo de un df que viene con gpd
-world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+#genero el colormap discreto
+cmap = cm.get_cmap('plasma_r', 11)
 
 # para ver solo sudamerica
 ax = world[world.continent == "South America"].plot(
-    edgecolor="black", figsize=(10, 10), column="name", cmap="Greens"
+    edgecolor="black", figsize=(10, 10),color='white'
 )
 
-# We can now plot our ``GeoDataFrame``.
-area_select = geodf_native[geodf_native.Macroarea == "South America"]
+#me quedo solo con la macroarea
+area_select = geodf[geodf.Macroarea == "South America"]
 
+#para plotear por tamaño
+pointsize = geodf['num_speakers'].tolist()
+pointsize = np.log(pointsize)*10
+
+#ahora si ploteo
 area_select.plot(
-    ax=ax, marker="o", markersize=15, alpha=0.5, column="num_speakers", legend=True
+    ax=ax, marker="o", markersize=pointsize, alpha=0.5, column="num_status", legend=True,
+    cmap=cmap
 )
 
 plt.show()
-# endregion
+#endregion
+
 #%%
-# region MUNDIAL MARCANDO DE DONDE ES C.IDIOMA (no funca porque se solapan los puntos)
+#region NEAREST LANG
+
+geod = pyproj.Geod(ellps = 'WGS84')
+
+def lang_dist(lang1, lang2, geod = geod):
+    lat_inicial = lang1.y # El eje y es la latitud
+    long_inicial = lang1.x # El eje x es la longitud
+
+    lat_final = lang2.y
+    long_final = lang2.x
+
+    un_angulo, otro_angulo, distancia =  geod.inv(lat_inicial,
+                                                  long_inicial,
+                                                  lat_final,
+                                                  long_final)
+    return distancia
+#%%
+#(aparte porque tarda mil años en correr)
+lang = geodf['WALS_ID'].tolist()
+dots = geodf['point_lang'].tolist()
+nearest = []
+
+n = 0
+for i, dot1 in tqdm(enumerate(dots)):
+    near = 0
+    radius = 100_000 #en metros
+    for j, dot2 in enumerate(dots):
+        if i!=j:
+            dist = lang_dist(dot1,dot2)
+            if dist < radius:
+                near +=1
+    n +=1
+    print(lang[i], near)
+    nearest.append(near)
+#endregion
+
+#%%
+#region FOLIUM MAP 2
+cdanger = [6.5,7,8,8.5,9,9.5]
+cext = 10
+gdanger = geodf[df['num_status'].apply(lambda x: x in cdanger)]
+gext = geodf[geodf.num_status == 10]
+gsafe = geodf[geodf.Status_binario == 1]
+
+m = folium.Map([-15.783333, -47.866667],
+                  zoom_start=4,
+                  tiles='cartodbpositron')
+
+heatmap_layer = FeatureGroup(name='Heatmap')
+
+HeatMap(data=gdanger[['Latitude_lang', 'Longitude_lang']], radius=15, min_opacity=0.4,
+ blur = 10).add_to(heatmap_layer.add_to(m))
+
+
+for idx, row in gdanger.iterrows():
+    dot = CircleMarker([row['Latitude_lang'], row['Longitude_lang']], radius=3,
+    tooltip = f"{gdanger.loc[idx,'Name']}, {gdanger.loc[idx,'Status']}",
+    color='black')
+    dot.add_to(m)
+
+feature_group_safe = FeatureGroup(name='No amenazados', show=False)
+
+for idx, row in gsafe.iterrows():
+    dot = CircleMarker([row['Latitude_lang'], row['Longitude_lang']], radius=3,
+    tooltip = f"{gsafe.loc[idx,'Name']}, {gsafe.loc[idx,'Status']}",
+    color='black',
+    alpha=0.5)
+    feature_group_safe.add_child(dot)
+
+
+feature_group_ext = FeatureGroup(name='Extintos')
+
+for idx, row in gext.iterrows():
+    dot = CircleMarker([row['Latitude_lang'], row['Longitude_lang']], radius=3,
+    tooltip = f"{gext.loc[idx,'Name']}, {gext.loc[idx,'Status']}",
+    color='black',
+    alpha=0.5)
+    feature_group_ext.add_child(dot)
+
+
+m.add_child(feature_group_ext)
+m.add_child(feature_group_safe)
+m.add_child(folium.map.LayerControl())
+#endregion
+#%%
+#region PLOTLY
+#pointsize = geodf['num_speakers']
+pointsize = np.log(geodf['num_speakers']+1)
+geodf['numlog'] = pointsize
+
+import plotly.express as px
+fig = px.scatter_mapbox(geodf,
+                        lat=geodf.geometry.y,
+                        lon=geodf.geometry.x,
+                        color="num_status", 
+                        size="numlog",
+                        color_continuous_scale=px.colors.cyclical.IceFire, 
+                        size_max=15, 
+                        hover_name='Name',
+                        text=geodf['num_speakers'],
+                        zoom=1)
+
+
+fig.update_traces(hovertemplate=None)
+fig.update_layout(mapbox_style="open-street-map")
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+#fig.update_traces(hovertemplate=f'Número de hablantes: %{size}')
+
+fig.show()
+
+
+#fig.write_html("/home/ingrid/Documents/labodatos/TP_final/df_principal/geoloc/speakers_riesgo.html")
+#%%
+#region cosas
 world = gpd.read_file(gpd.datasets.get_path("naturalearth_lowres"))
+query_official = pd.read_csv('/home/ingrid/Documents/labodatos/TP_final/df_principal/query_final.csv')
+df_test = world.copy()
 
-# We restrict to South America.
-ax = world.plot(edgecolor="black", figsize=(10, 10), column="continent")
+mapita1 = query_official.set_index("countryISO").to_dict()["ISO"]
+mapita1['USA'] = 'eng'
+mapita1['MEX'] = 'spa'
+df["official_lang_iso"] = df["Native_country_ISO"].replace(mapita1)
 
-# We can now plot our ``GeoDataFrame``.
-geodf_ext.plot(ax=ax, column="Native_country", marker="o", markersize=15, alpha=0.5)
 
-plt.show()
-# endregion
 #%%
-# region MAPA NORTEAMERICA NAVITOS + EXT
+#cat = ['6b Threatened', '4 Educational', '5 Developing','6a Vigorous', '7 Shifting']
+#df_cat = df[df['Status'].apply(lambda x: x in cat)]
 
-# We restrict to South America.
-ax = world[world.continent == "North America"].plot(
-    edgecolor="black", figsize=(8, 8), color="white"
-)
+idiso = pd.read_csv('/home/ingrid/Documents/labodatos/TP_final/df_principal/iso_to_id.csv')
 
-# We can now plot our ``GeoDataFrame``.
-area_select = geodf_native[geodf_native.Macroarea == "North America"]
+mapid = idiso.set_index('countryID').to_dict()['countryISO']
+mappbi = world.set_index("iso_a3").to_dict()["gdp_md_est"]
 
-area_select.plot(
-    ax=ax,
-    marker="o",
-    markersize=30,
-    alpha=0.5,
-    column="num_speakers",
-    cmap="plasma_r",
-    legend=True,
-    legend_kwds={"shrink": 0.3},
-)
+df['Native_country_ISO'] = df['Native_country'].replace(mapid).apply(lambda x: x.upper())
 
-externos = geodf_ext[geodf_ext.Macroarea == "North America"]
-externos.plot(ax=ax, marker="o", color="red")
-
-plt.show()
-# endregion
+df['PBI_native'] = df['Native_country_ISO'].replace(mappbi)
 # %%
-ax = world[world.continent == "Africa"].plot(
-    edgecolor="black", figsize=(8, 8), color="white"
-)
-
-
-area_select = geodf_native[geodf_native.Macroarea == "Africa"].sort_values(
-    "num_speakers"
-)
-
-area_select.plot(
-    ax=ax,
-    marker="o",
-    markersize=30,
-    alpha=0.3,
-    column="num_speakers",
-    cmap="plasma_r",
-    legend=True,
-)
-
-# externos = geodf2[geodf2.Macroarea == "Europe"]
-# externos.plot(ax=bordes, marker='o',color='black')
-
-plt.show()
-# %%
+mandar = df[['ISO','WALS_ID','Native_country','Native_country_ISO','PBI_native','official_lang_iso']]
+mandar.to_csv('pbi_oficial_lang.csv')
